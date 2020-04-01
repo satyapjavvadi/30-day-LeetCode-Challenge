f45e67113f5eb9a42c232f9320ce7a4c50befc7c
Satya P Javvadi <saphin.rao@gmail.com>
2020-04-01 16:21:14 -0700

    Add project files.

---
+++ FindSingleNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FindSingleNumber
+{
+    class Program
+    {
+        public static int FindSinNumber(int[] nums)
+        {
+            int res = nums[0];
+            for(int i = 1; i < nums.Length; i++)
+            {
+                res = res ^ nums[i];
+            }
+            return res;
+        }
+        public static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 2 };
+            int singNumber = FindSinNumber(nums);
+            Console.WriteLine(singNumber);
+        }
+    }
+}
