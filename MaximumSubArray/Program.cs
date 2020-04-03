9ec81b1f8e5568560958c2c045b8c2a9d21cf0cd
Satya P Javvadi <saphin.rao@gmail.com>
2020-04-03 16:08:18 -0700

    Added MaximumSubArray problem

---
+++ MaximumSubArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MaximumSubArray
+{
+    class Program
+    {
+        public static int MaximumSumArray(int[] nums)
+        {
+            int max_sum = nums[0];
+            int sum = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if(max_sum < sum)
+                {
+                    max_sum = sum;
+                }
+                if(sum < 0)
+                {
+                    sum = 0;
+                }
+            }
+            return max_sum;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int max_sum = MaximumSumArray(nums);
+            Console.WriteLine(max_sum);
+        }
+    }
+}
