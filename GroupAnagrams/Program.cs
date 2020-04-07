656463917e5a654a2a3872f71e4939debf26b69d
Satya P Javvadi <saphin.rao@gmail.com>
2020-04-06 23:36:28 -0700

    Added GroupAnagrams program

---
+++ GroupAnagrams/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GroupAnagrams
+{
+    class Program
+    {
+        public static IList<IList<string>> PrintAnagrams(string[] strs)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+            foreach (var str in strs)
+            {
+                var ch = str.ToCharArray();
+                Array.Sort(ch);
+                var key = new string(ch);
+
+                if (!dict.ContainsKey(key))
+                {
+                    dict[key] = new List<string>();
+                }
+                dict[key].Add(str);
+            }
+            var list = new List<IList<string>>();
+            foreach (var kv in dict)
+            {
+                list.Add(kv.Value);
+            }
+            return list;
+
+        }
+        static void Main(string[] args)
+        {
+            string[] input =  { "eat", "tea", "tan", "ate", "nat", "bat" };
+            var output = PrintAnagrams(input);            
+        }
+    }
+}
