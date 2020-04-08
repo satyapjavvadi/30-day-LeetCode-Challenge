2a3c94755f06d4c7e2366111b706ef3b4ab1cbc9
Satya P Javvadi <saphin.rao@gmail.com>
2020-04-07 20:24:53 -0700

    Added CountElements program

---
+++ CountingElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountingElements
+{
+    class Program
+    {
+        public static int CountingElements(int[] arr)
+        {
+            var dict = new Dictionary<int, int>();
+            int count = 0;
+
+                for(int i = 0; i < arr.Length; i++)
+                {
+                   if(!dict.ContainsKey(arr[i]))
+                   {
+                     dict.Add(arr[i], 1);
+                   }
+                   else
+                   {
+                    dict[arr[i]]++;
+                   }
+
+                }
+
+                for(int i = 0; i < arr.Length; i++)
+                {
+                   if(dict.ContainsKey(arr[i] + 1))
+                   {
+                    count++;
+                   }
+                }
+
+                return count;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            int b = CountingElements(arr);
+            Console.WriteLine($"b: {b}");
+        }
+    }
+}
