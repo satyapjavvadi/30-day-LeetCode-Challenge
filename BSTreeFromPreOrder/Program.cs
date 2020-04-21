2162d99c7580f2d3e8701811935e7a72d9042c85
Satya P Javvadi <saphin.rao@gmail.com>
2020-04-20 23:27:10 -0700

    Added BSTreeFromPreOrder program

---
+++ BSTreeFromPreOrder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BSTreeFromPreOrder
+{
+    class Program
+    {
+        public class TreeNode
+        {
+         public int val;
+         public TreeNode left;
+         public TreeNode right;
+         public TreeNode(int x) { val = x; }
+        }
+        public TreeNode BstFromPreorder(int[] preorder)
+        {
+            return BSTTree(preorder, 0, preorder.Length - 1);
+        }
+        public TreeNode BSTTree(int[] preorder, int l, int r)
+        {
+
+            if (l > r)
+            {
+                return null;
+            }
+
+            TreeNode root = new TreeNode(preorder[l]);
+            if (l == r)
+            {
+                return root;
+            }
+
+            int nextEle = l + 1;
+            while (nextEle <= r && preorder[nextEle] < preorder[l])
+            {
+                nextEle++;
+            }
+
+            root.left = BSTTree(preorder, l + 1, nextEle - 1);
+            root.right = BSTTree(preorder, nextEle, r);
+
+            return root;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
