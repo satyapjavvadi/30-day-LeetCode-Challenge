14f6fde941032b544e799a54d63fae19042a8ff0
Satya P Javvadi <saphin.rao@gmail.com>
2020-04-22 22:39:08 -0700

    Added SubArraySumEqualsK progarm

---
+++ SubArrayAumEqualsK/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SubArrayAumEqualsK
+{
+    class Program
+    {
+        public static int SubarraySum(int[] nums, int k)
+        {
+
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    sum = sum + nums[j];
+                    if (sum == k)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 1, 1 };
+            int k = 2;
+            int count = SubarraySum(arr, k);
+            Console.WriteLine(count);
+        }
+    }
+}
